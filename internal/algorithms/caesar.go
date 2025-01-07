b844dbbf1e11cf4747ad48db2e7797af435bfb81
Eric Yager <ejyager00@protonmail.com>
2025-01-07 09:44:20 -0500

    Standardize util function naming

--- internal/algorithms/caesar.go
+++ internal/algorithms/caesar.go
@@ -8,9 +8,9 @@ import "github.com/ejyager00/gocrypt/internal/util"
 func CaesarEncrypt(cleartext string, shift uint8) string {
 	b := []byte(cleartext)
 	for i, x := range b {
-		if util.IsUpperCase(x) {
+		if util.IsUpper(x) {
 			b[i] = ((x-65)+shift)%26 + 65
-		} else if util.IsLowerCase(x) {
+		} else if util.IsLower(x) {
 			b[i] = ((x-97)+shift)%26 + 97
 		} else {
 			b[i] = x
--- internal/util/textutil.go
+++ internal/util/textutil.go
@@ -3,10 +3,34 @@ Copyright © 2025 Eric Yager
 */
 package util
 
-func IsUpperCase(b byte) bool {
+func IsUpper(b byte) bool {
 	return b >= 'A' && b <= 'Z'
 }
 
-func IsLowerCase(b byte) bool {
+func IsLower(b byte) bool {
 	return b >= 'a' && b <= 'z'
+}
+
+func AllUpper(text string) string {
+	b := []byte(text)
+	for i, x := range b {
+		if x >= 'a' && x <= 'z' {
+			b[i] = x - 32
+		} else {
+			b[i] = x
+		}
+	}
+	return string(b)
+}
+
+func AllLower(text string) string {
+	b := []byte(text)
+	for i, x := range b {
+		if x >= 'A' && x <= 'Z' {
+			b[i] = x + 32
+		} else {
+			b[i] = x
+		}
+	}
+	return string(b)
 }
