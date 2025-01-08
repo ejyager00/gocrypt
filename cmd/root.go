c7e4a732861ff1be5e80c32d980d74a299b36074
Eric Yager <ejyager00@protonmail.com>
2025-01-08 09:23:25 -0500

    Implement CLA flags and args

--- cmd/root.go
+++ cmd/root.go
@@ -4,17 +4,66 @@ Copyright © 2025 Eric Yager
 package cmd
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/ejyager00/gocrypt/internal/algorithms"
 	"github.com/spf13/cobra"
 )
+
+var encrypt bool
+var decrypt bool
+var inFile string
+var outFile string
+var key string
+var caesar bool
+var vigenere bool
 
 var rootCmd = &cobra.Command{
 	Use:   "gocrypt",
 	Short: "A simple message encryption CLA.",
 	Long: `A simple message encryption CLA. Includes simple message ciphers using 
 	caesar and vigenere strategies.`,
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		if !encrypt && !decrypt {
+			cmd.Help()
+			return
+		}
+
+		if !caesar && !vigenere {
+			cmd.Help()
+			return
+		}
+
+		content, err := os.ReadFile(inFile)
+		if err != nil {
+			fmt.Println("Error reading input file:", err)
+			return
+		}
+
+		var result string
+		if caesar {
+			shift := uint8(0)
+			fmt.Sscan(key, &shift)
+			if encrypt {
+				result = algorithms.CaesarEncrypt(string(content), shift)
+			} else {
+				result = algorithms.CaesarDecrypt(string(content), shift)
+			}
+		} else {
+			if encrypt {
+				result = algorithms.VigenereEncrypt(string(content), key)
+			} else {
+				result = algorithms.VigenereDecrypt(string(content), key)
+			}
+		}
+
+		err = os.WriteFile(outFile, []byte(result), 0644)
+		if err != nil {
+			fmt.Println("Error writing output file:", err)
+			return
+		}
+	},
 }
 
 func Execute() {
@@ -25,5 +74,15 @@ func Execute() {
 }
 
 func init() {
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().BoolVarP(&encrypt, "encrypt", "e", false, "Encrypt the input")
+	rootCmd.Flags().BoolVarP(&decrypt, "decrypt", "d", false, "Decrypt the input")
+	rootCmd.Flags().StringVarP(&inFile, "input", "i", "", "Input file path")
+	rootCmd.Flags().StringVarP(&outFile, "output", "o", "", "Output file path")
+	rootCmd.Flags().StringVarP(&key, "key", "k", "", "Encryption/decryption key")
+	rootCmd.Flags().BoolVarP(&caesar, "caesar", "c", false, "Use Caesar cipher")
+	rootCmd.Flags().BoolVarP(&vigenere, "vigenere", "v", false, "Use Vigenere cipher")
+
+	rootCmd.MarkFlagRequired("input")
+	rootCmd.MarkFlagRequired("output")
+	rootCmd.MarkFlagRequired("key")
 }
